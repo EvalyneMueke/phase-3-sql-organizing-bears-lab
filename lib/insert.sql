413d55bf82de5829bd122c7033768998bc02c796
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-02-16 10:03:41 +0300

    Completed organizing bear lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Mr.Chocolate", 4, "F", "Brown", "shy",true );
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Rowdy", 9, "M", "Black", "doninance",false);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Tabitha", 7, "F", "white", "outgoing",true);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Sergeant Brown", 3, "F", "Brown", "friendly", true);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Melissa", 6, "F", "white", "skittish", false);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Grinch", 1, "M", "Black", "snuggly",true);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Wendy", 2, "F", "grey", "relaxed", true);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES(NULL,5, "M", "golden","independent",true);
--- lib/seed.sql
+++ lib/seed.sql
@@ -7,3 +7,16 @@ INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
 (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
 (8,null, 20, "M", "black", "aggressive", 0);
+
+SELECT bears.name, bears.age FROM bears WHERE bears.sex="F";
+
+SELECT bears.name FROM bears ORDER BY bears.name;
+
+SELECT bears.name, bears.age FROM bears WHERE bears.alive=1 ORDER BY bears.age;
+
+SELECT bears.name, bears.age FROM bears ORDER BY bears.age DESC LIMIT 1;
+
+SELECT bears.name, bears.age FROM bears ORDER BY bears.age ASC LIMIT 1;
+
+
+
